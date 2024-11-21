6935f242f69357ba898378851a1a013bea968ed4
TylerAgostino <tyleragostino@gmail.com>
2024-11-20 19:45:39 -0500

    fix reloading bug and add black flag reasons

--- modules/page_sources/sprint_race_dq.py
+++ modules/page_sources/sprint_race_dq.py
@@ -4,25 +4,22 @@ import streamlit as st
 import uuid
 from modules.events.random_timed_event import TimedEvent
 
-if 'dq_cars' not in st.session_state:
-    st.session_state.dq_cars = [
-        {'id': uuid.uuid4(), 'car_number': 19},
-        {'id': uuid.uuid4(), 'car_number': 99}
-    ]
 
 class SprintRaceDQEvent(TimedEvent):
     def __init__(self, cars, penalty, *args, **kwargs):
         self.cars = cars
         self.penalty = penalty
+        self.reason = self.generate_random_black_flag_reason()
         super().__init__(*args, **kwargs)
 
     def event_sequence(self):
+        self._chat(f'Black Flag: {self.reason}', race_control=True)
         for car in self.cars:
             self._chat(f'!bl {car["car_number"]} {self.penalty}')
 
 def start_sequence():
     with st.status('DQ Bot Armed'):
-        dq_event = SprintRaceDQEvent(st.session_state.dq_cars, event_time=st.session_state.dq_time)
+        dq_event = SprintRaceDQEvent(st.session_state.dq_cars, event_time=st.session_state.dq_time, penalty=st.session_state.dq_penalty)
         thread = threading.Thread(target=dq_event.run)
         st.session_state.bot = dq_event
         thread.start()
@@ -32,6 +29,11 @@ def end_sequence():
         st.session_state.bot.cancel_event.set()
 
 def ui():
+    st.session_state.setdefault('dq_cars', [
+        {'id': uuid.uuid4(), 'car_number': 19},
+        {'id': uuid.uuid4(), 'car_number': 99}
+    ])
+    st.session_state.setdefault('dq_time', -60)
     st.header("Disqualification Settings")
 
     col1, col2, col3, _ = st.columns([1, 1, 1, 3])
