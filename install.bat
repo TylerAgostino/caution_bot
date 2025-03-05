997d2e983682dee69a9e870d61833f4b568bbe18
TylerAgostino <tyleragostino@gmail.com>
2025-03-05 18:38:12 -0500

    batch file pathing fixes

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 del /f /q bot_venv
 python -m venv bot_venv
 call bot_venv\Scripts\activate
-call pip install -r ../../requirements.txt
+call pip install -r requirements.txt
 ECHO Done
--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 call bot_venv/Scripts/activate
-python -m streamlit run ../../better_caution_bot.py
+python -m streamlit run better_caution_bot.py
