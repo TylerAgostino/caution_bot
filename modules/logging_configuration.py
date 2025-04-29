7578a7630ab7a0a7f01608e040b388aea32fd85c
TylerAgostino <tyleragostino@gmail.com>
2025-04-29 09:16:43 -0400

    make event type optional for loggers to reduce stdout spam

--- modules/logging_configuration.py
+++ modules/logging_configuration.py
@@ -1,6 +1,18 @@
 import logging
 from logging.config import dictConfig
 import os
+import re
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        arg_pattern = re.compile(r'%\((\w+)\)')
+        arg_names = [x.group(1) for x in arg_pattern.finditer(self._fmt)]
+        for field in arg_names:
+            if field not in record.__dict__:
+                record.__dict__[field] = None
+
+        return super().format(record)
+
 
 def init_logging(level='INFO'):
     """
@@ -13,7 +25,8 @@ def init_logging(level='INFO'):
         'version': 1,
         'formatters': {
             'default': {
-                'format': '%(asctime)s - %(event)s - %(levelname)s - %(message)s \f'
+                'format': '%(asctime)s - %(event)s - %(levelname)s - %(message)s \f',
+                'class': 'modules.logging_configuration.CustomFormatter',
             }
         },
         'handlers': {
