f26d28e4a776c4f2d5bf5d955cc843bc66b0000d
TylerAgostino <tyleragostino@gmail.com>
2025-06-02 20:32:33 -0400

    atvo text consumer

--- modules/events/text_consumer_event.py
+++ modules/events/text_consumer_event.py
@@ -1,3 +1,5 @@
+from signalr import Connection
+from requests import Session
 import queue
 from modules.events.base_event import BaseEvent
 from ws4py.client.threadedclient import WebSocketClient
@@ -10,7 +12,8 @@ class TextConsumerEvent(BaseEvent):
     """
     Consumes text messages to be displayed on the Broadcast through the SDKGaming Websocket
     """
-    def __init__(self, password: str = '', room: str = '', test=False, sdk=None, *args, **kwargs):
+    def __init__(self, password: str = '', room: str = '',
+                 test=False, sdk=None, *args, **kwargs):
         self.password = password
         self.room = room
         super().__init__(sdk=sdk, *args, **kwargs)
@@ -116,3 +119,63 @@ class DiscordTextConsumerEvent(TextConsumerEvent):
             'Text Channel ID': col2.text_input("Text Channel ID", key=f'{ident}room', value=''),
             'test': col1.checkbox("Test", key=f'{ident}test', value=False)
         }
+
+class ATVOTextConsumerEvent(TextConsumerEvent):
+    from enum import Enum
+    class EntryIdType(Enum):
+        CarIdx = 0
+        CarNumber = 1
+        CustomerId = 2
+    class DecisionType(Enum):
+        NoDecision = 0
+        Cleared = 1
+        NoFurtherAction = 2
+        Warning = 3
+        Penalty = 4
+    class PenaltyTypes(Enum):
+        NoPenalty = 0
+        SwapPosition = 1
+        DriveThrough = 2
+        StopAndGo = 3
+        TimePenalty = 4
+        Disqualify = 5
+    class MessageTypes(Enum):
+        Unknown = 0
+        Info = 1
+        Warning = 2
+        Penalty = 3
+        Investigation = 4
+        NoFurtherAction = 5
+        ClearPenalty = 6
+
+    @staticmethod
+    def ui(ident=''):
+        import streamlit as st
+        col1, col2 = st.columns(2)
+        return {
+            'password': col1.text_input("Password", key=f'{ident}password', value=''),
+            'test': col1.checkbox("Test", key=f'{ident}test', value=False)
+        }
+
+    def send_message(self, text: dict):
+        """
+        Sends text to the queue.
+        """
+        self.logger.debug('Sending message to ATVO')
+        #create a connection
+        connection = Connection("http://localhost:1337/signalr", Session())
+
+        #get chat hub
+        chat = connection.register_hub('RaceControlHub')
+
+        # #start a connection
+        connection.start()
+
+        chat.server.invoke('sendMessage', {
+            'source': "Better Caution Bot",
+            'type': '1',
+            # 'entryId': 3, # Optional entry CarIdx, leave out if not related to anyone
+            'sessionName': "Race",
+            'header': text['title'],
+            'text': text['text'],
+        })
