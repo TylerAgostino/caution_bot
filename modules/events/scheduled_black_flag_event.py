274e3db3f6b446e052e6c09f80e844edc57e1e42
Tyler Agostino <tyleragostino@gmail.com>
2026-01-12 16:33:46 -0500

    remove bl reason

--- modules/events/scheduled_black_flag_event.py
+++ modules/events/scheduled_black_flag_event.py
@@ -5,11 +5,9 @@ class SprintRaceDQEvent(TimedEvent):
     def __init__(self, cars, penalty, *args, **kwargs):
         self.cars = cars
         self.penalty = penalty
-        self.reason = self.generate_random_black_flag_reason()
         super().__init__(*args, **kwargs)
 
     def event_sequence(self):
-        self._chat(f"Black Flag: {self.reason}", race_control=True)
         if isinstance(self.cars, str):
             cars = self.cars.split(",")
         else:
